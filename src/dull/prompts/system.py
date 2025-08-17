3461123e6276f5324239bd10fc52d953ac4f9f56
johncllstokes <johnclstokes@hotmail.com>
2025-08-17 20:08:13 +0100

    reformatted the schema using a json file hopefully thats a bit more easily managed

--- src/dull/prompts/system.py
+++ src/dull/prompts/system.py
@@ -1,4 +1,12 @@
-prompt = """you are a linting tool that will be used for either python code projects or technical documentation. your purpose is not to focus on 'code quality', spelling, grammar etc... but rather on ensuring the architecture of the solution is inline with the technical strategy of the organisation. This technical strategy is described using the following rules, which you are to interpret as plain text.
+import json
+
+
+with open('schema.json', 'r') as f:
+    schema = json.load(f)
+    
+formatted_schema = json.dumps(schema, indent=2)
+
+prompt = f"""you are a linting tool that will be used for either python code projects or technical documentation. your purpose is not to focus on 'code quality', spelling, grammar etc... but rather on ensuring the architecture of the solution is inline with the technical strategy of the organisation. This technical strategy is described using the following rules, which you are to interpret as plain text.
 ```
 {}
 ```
@@ -6,5 +14,12 @@ you are also provided the following additional free text context by the organisa
 ```
 {}
 ```
-scan the code for violations of the provided strategic rules, keeping the additional context in mind. respond using the following schema, keeping to multiple separate violations of each code where a user repeatedly commits the sin rather than lumping them all into one: { "strategic_violations": [ { "rule_code": str, # please use multiple structs if a certain rule is violated multiple times "violating_snippets": [ { "snippet": str, "explanation": str } ], "suggested_alternative": str } ] } the code for you to scan is as follows:
+scan the code for violations of the provided strategic rules, keeping the additional context in mind. respond using the following schema, keeping to multiple separate violations of each code where a user repeatedly commits the sin rather than lumping them all into one: 
+```
+{formatted_schema}
+```
+the code for you to scan is as follows:
+```
+{}
+```
 """
