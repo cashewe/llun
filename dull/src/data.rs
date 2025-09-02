539703b2ca78fdd0ecb61c7228926857a762b9ec
JohnCLlStokes <johnclstokes@hotmail.com>
2025-09-02 21:57:49 +0000

    moved data into src to be consistent with convention

--- dull/src/data.rs
+++ dull/src/data.rs
@@ -1,4 +1,4 @@
 use include_dir::{include_dir, Dir};
 
-pub static RULES_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/data/rules");
-pub const DEFAULT_RULES: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/data/default_rules.txt"));
+pub static RULES_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/src/data/rules");
+pub const DEFAULT_RULES: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "src/data/default_rules.txt"));
