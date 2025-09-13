511439e562954d84a22e3fcd5e1e7f58a90a5754
JohnCLlStokes <johnclstokes@hotmail.com>
2025-09-13 00:38:28 +0000

    lill figure this out in the morning

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,6 @@
 use include_dir::{include_dir, Dir};
 
+pub static DEFAULT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/src/data/");
 pub static RULES_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/src/data/rules");
 pub static PROMPT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/src/data/prompts");
 pub const DEFAULT_RULES: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/data/default_rules.txt"));
