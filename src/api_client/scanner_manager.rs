d501b9ba7c869aa91d941302789c68159da0dbf2
JohnCLlStokes <johnclstokes@hotmail.com>
2025-09-20 12:11:17 +0000

    added a crappy multi-runner, will need to implement the cleanup method to lock the method in. id also like to tidy the code from being three manual calls...

--- src/api_client/scanner_manager.rs
+++ src/api_client/scanner_manager.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use tokio::try_join;
 use crate::api_client::{AvailableScanner, OpenAiClientError, OpenAiPublicScanner, Response, Scanner, ScannerError};
 
 
@@ -27,11 +28,33 @@ impl ScannerManager {
 
     /// use your chosen scanner (its open ai isnt you normie)
     /// to perform a scan
-    pub async fn run_scan(&self, system_prompt: &str, user_prompt: &str, model: String, scanner: AvailableScanner) -> Result<Response, ScannerManagerError> {
+    pub async fn run_scan(&self, system_prompt: &str, user_prompt: &str, model: String, scanner: AvailableScanner, production_mode: bool) -> Result<Response, ScannerManagerError> {
         let chosen_scanner = self.scanners
             .get(&scanner)
             .ok_or_else(ScannerManagerError::ScannerNotFound)?;
-            
-        Ok( chosen_scanner.scan_files(system_prompt, user_prompt, model).await? )
+
+        if production_mode {
+            let (response1, response2, response3) = try_join!(
+                chosen_scanner.scan_files(system_prompt, user_prompt, model.clone()),
+                chosen_scanner.scan_files(system_prompt, user_prompt, model.clone()),
+                chosen_scanner.scan_files(system_prompt, user_prompt, model.clone())
+            )?;
+
+            let combined = self.combine_responses(vec![response1, response2, response3]);
+
+            Ok( combined )
+        } else{
+            println!("Consider using production mode if reliably reproducible results are desired");
+            Ok( chosen_scanner.scan_files(system_prompt, user_prompt, model).await? )
+        }
+    }
+
+    /// merge many async responses into a single Response object
+    fn combine_responses(&self, responses: Vec<Response>) -> Response {
+        let mut all_issues = Vec::new();
+        for response in responses {
+            all_issues.extend(response.detected_issues);
+        }
+        Response { detected_issues: all_issues }
     }
 }
