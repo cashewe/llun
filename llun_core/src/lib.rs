3ae2fe66747595486fa12db3fb721f67eab1b726
JohnCLlStokes <johnclstokes@hotmail.com>
2025-09-28 21:22:36 +0000

    refactored to depend on shared library in anticipation of new toys

---
+++ llun_core/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod api_client;
+pub mod data;
+pub mod files;
+pub mod formatters;
+pub mod rules;
+pub mod per_file_ignorer;
+
+pub use api_client::{AvailableScanner, PromptManager, ScannerManager};
+pub use data::DEFAULT_CONFIG;
+pub use files::FileManager;
+pub use formatters::{OutputFormat, OutputManager};
+pub use rules::RuleManager;
+pub use per_file_ignorer::PerFileIgnorer;
